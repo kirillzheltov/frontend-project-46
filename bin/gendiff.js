c8eadc0aa1b4bda84e9b4fe411682e9422e4c3db
Kirill Zheltov <kirillzheltov@gmail.com>
2024-03-07 00:30:56 -0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,9 @@ import { program } from 'commander';
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0');
+  .version('0.8.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
